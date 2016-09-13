e965c0146b25c431aef8a18b781e774812327e14
Lucas Casagrande <lcasagrande@itba.edu.ar>
2016-09-13 10:11:38 -0300

    Agregue lo que se vio en clase ayer de webconfig.
    Ahora se muestran las flechas para desplazarse por el mapa, pero todavia no hacen nada.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -6,16 +6,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
 
 @EnableWebMvc
 @ComponentScan({"ar.edu.itba.paw.webapp.controller","ar.edu.itba.service","ar.edu.itba.persistance"})
 @Configuration
-public class WebConfig {
-	
+public class WebConfig extends WebMvcConfigurerAdapter {
+
 	@Bean
 	public ViewResolver viewResolver(){
 		final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -25,13 +31,35 @@ public class WebConfig {
 
 		return viewResolver;
 	}
-	
+
+
 	public DataSource dataSource(){
-		
+
 		final SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 		dataSource.setDriverClass(org.postgresql.Driver.class);
-	  	dataSource.setUrl("jdbc:postgresql://localhost/paw");
-	  	
-	  	return dataSource;
+		dataSource.setUrl("jdbc:postgresql://localhost/paw");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+
+	public DataSourceInitializer dataSourceInitializer (final DataSource ds){
+		DataSourceInitializer dsi= new DataSourceInitializer();
+		dsi.setDataSource(ds);
+		dsi.setDatabasePopulator(databasePopulator());
+		return dsi;
+
+	}
+
+	private DatabasePopulator databasePopulator(){
+		final ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
+		//dbp.addScript(schemaSql);
+		return dbp;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+		registry.addResourceHandler("/img/**").addResourceLocations("/resources/images/");
 	}
 }
--- webapp/src/main/webapp/WEB-INF/jsp/index.jsp
+++ webapp/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,15 +1,10 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="te" uri="custom.tld"%>
 <%@ include file="header.jsp" %>
+<link rel="stylesheet" href="<c:url value= "/resources/css/style.css" /> ">
 
-<table>
-	<tr>
-	<div class="btn-group btn-group-justified" role="group" aria-label="...">
-		<div class="btn-group" role="group">
-			<button type="button" class="btn btn-default">UP</button>
-		</div>
-	</div>
-	</tr>
+<div id="myCarousel" class="carousel slide" data-ride="carousel" style="display: inline-block">
+<table style="max-width: 896px;">
 <c:forEach var="row" items="${map}">
 <tr>
 	<c:forEach var="item" items="${row}">
@@ -18,7 +13,22 @@
 </tr>
 </c:forEach>
 </table>
-
-<c:out value="TOMA PUTO" />
+	<a class="left carousel-control" href="#myCarousel" role="button">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Left</span>
+	</a>
+	<a class="up carousel-control" href="#myCarousel" role="button">
+		<span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+		<span class="sr-only">Up</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" role="button">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Right</span>
+	</a>
+	<a class="down carousel-control" href="#myCarousel" role="button">
+		<span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
+		<span class="sr-only">Down</span>
+	</a>
+</div>
 
 <%@ include file="footer.jsp" %>
