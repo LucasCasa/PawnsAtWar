1c7253d8285e9d3d178f99b83d8f17940bf90bc8
root <root@localhost.localdomain>
2016-09-12 21:35:30 -0300

    webapp/pom.xml dependencies fixed

---
+++ interfaces/src/main/java/ar/edu/itba/interfaces/TerrainService.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.interfaces;
+
+/**
+ * Created by root on 9/12/16.
+ */
+public interface TerrainService {
+}
---
+++ service/src/main/java/ar/edu/itba/service/TerrainServiceImpl.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.service;
+
+/**
+ * Created by root on 9/12/16.
+ */
+public class TerrainServiceImpl {
+}
