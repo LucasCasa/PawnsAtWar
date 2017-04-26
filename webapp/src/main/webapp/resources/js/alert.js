2ed97c1889f150adab41dc0d576a177fe028e3eb
Maggie Vega <mvega@itba.edu.ar>
2017-04-26 09:03:24 -0300

    No muestra mas números negativos

--- webapp/src/main/webapp/resources/js/alert.js
+++ webapp/src/main/webapp/resources/js/alert.js
@@ -5,8 +5,11 @@ var intervalID = setInterval(function(){
     $('.timeR').each(function(i, obj) {
         value = parseInt($(this).text());
             $(this).text(value -1);
-        if($(this).text() == -1){
+        if($(this).text() == 0){
             location.reload(false);
+        }
+        if($(this).text() <0){
+            $(this).text(0);
         }
     });
 }, 1000);
