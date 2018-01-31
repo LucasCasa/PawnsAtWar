c52d6c1c63a0c01ea78c52ce667d8dff97289234
nkuyumciyan <nkuyumciyan@itba.edu.ar>
2018-01-31 19:14:00 -0300

    Prefix api calls, Get all trades call and return # for invalid position in /map

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/DTOs/TradeOfferResourceDTO.java
@@ -0,0 +1,31 @@
+package ar.edu.itba.paw.webapp.DTOs;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TradeOfferResourceDTO {
+
+    private int type;
+    private int amount;
+
+    public TradeOfferResourceDTO(int type, int amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
--- webapp/src/main/java/ar/edu/itba/paw/webapp/data/Validator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/data/Validator.java
@@ -2,41 +2,49 @@ package ar.edu.itba.paw.webapp.data;
 
 import java.text.MessageFormat;
 
-/**
- * Created by lucas on 05/10/16.
- */
 public final class Validator {
 
-	private static final String basePath = "/map/{0}/{1}";
-	
-	private Validator(){
-		
-	}
+    private static final String basePath = "/map/{0}/{1}";
+
+    private Validator() {
+
+    }
 
     public static boolean validBoardPosition(String pos) {
-        if(isInteger(pos)){
-            if(Integer.parseInt(pos) <= Info.MAP_SIZE){
+        if (isInteger(pos)) {
+            if (Integer.parseInt(pos) <= Info.MAP_SIZE) {
                 return true;
             }
         }
         return false;
     }
-    public static boolean isInteger(String pos){
 
-        if(pos == null){
+    public static boolean isInteger(String pos) {
+
+        if (pos == null) {
             return false;
         }
         String regex = "0*\\d{1,9}";
         return pos.matches(regex);
     }
-	public static int getValidPos(int num) {
-		if(num<Info.VIEW_RANGE)
-			num = Info.VIEW_RANGE;
-		else if(num>Info.MAP_SIZE-Info.VIEW_RANGE)
-			num = Info.MAP_SIZE-Info.VIEW_RANGE;
-		return num;
-	}
-	public static String format(int x, int y) {
-    	return MessageFormat.format(basePath, x, y);
-	}
+
+    public static int getValidPos(int num) {
+        if (num < Info.VIEW_RANGE)
+            num = Info.VIEW_RANGE;
+        else if (num > Info.MAP_SIZE - Info.VIEW_RANGE)
+            num = Info.MAP_SIZE - Info.VIEW_RANGE;
+        return num;
+    }
+
+    private static boolean isValidPosToDisplay(int num) {
+        return num >= Info.VIEW_RANGE && num <= Info.MAP_SIZE - Info.VIEW_RANGE;
+    }
+
+    public static String format(int x, int y) {
+        if (isValidPosToDisplay(x) && isValidPosToDisplay(y)) {
+            return MessageFormat.format(basePath, x, y);
+        } else {
+            return "#";
+        }
+    }
 }
