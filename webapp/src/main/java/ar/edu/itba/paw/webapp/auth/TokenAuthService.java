5be94a5c55bf9a7721fa6afc80a11c7a2b80d968
Nicolas Kuyumciyan <nkuyumciyan@medallia.com>
2018-02-02 21:52:00 -0300

    Working authentication

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/TokenAuthService.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/TokenAuthService.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.webapp.auth;
 
 import ar.edu.itba.paw.webapp.DTOs.UserAuthDTO;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,7 +12,9 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 public class TokenAuthService {
@@ -48,9 +51,17 @@ public class TokenAuthService {
 
     Authentication getAuthForLogin(final HttpServletRequest request) {
         ObjectMapper mapper = new ObjectMapper();
+        //mapper.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, true);
 
-        try {
-            UserAuthDTO user = mapper.readValue(request.getInputStream(), UserAuthDTO.class);
+        try(BufferedReader reader = request.getReader()) {
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append('\n');
+            }
+
+            UserAuthDTO user = mapper.readValue(builder.toString(), UserAuthDTO.class);
             UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
 
             if (user.getPassword().equals(userDetails.getPassword())) {
