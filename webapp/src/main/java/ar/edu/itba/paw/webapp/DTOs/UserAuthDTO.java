a65d5939968e39d033890ea936a43ede44bbf0f2
Nicolas Kuyumciyan <nkuyumciyan@medallia.com>
2018-02-02 21:52:00 -0300

    Working authentication

--- webapp/src/main/java/ar/edu/itba/paw/webapp/DTOs/UserAuthDTO.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/DTOs/UserAuthDTO.java
@@ -4,11 +4,6 @@ public class UserAuthDTO {
     private String username;
     private String password;
 
-    public UserAuthDTO(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
     public String getUsername() {
         return username;
     }
