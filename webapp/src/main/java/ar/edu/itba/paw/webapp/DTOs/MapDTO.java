d260a5224fe9142d9a84e1a25dc38d398528ebb1
Nico Castano <nicolasm.castano@gmail.com>
2018-02-11 23:32:29 -0300

    TILE, MAP, COMMERCE.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/DTOs/MapDTO.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/DTOs/MapDTO.java
@@ -11,88 +11,87 @@ import java.util.List;
 public class MapDTO {
 
   private int id;
-	private int x;
-	private int y;
-	private String nextx;
-	private String nexty;
-	private String prevx;
-	private String prevy;
-	private List<List<TileDTO>> tiles;
+  private int x;
+  private int y;
+  private String nextx;
+  private String nexty;
+  private String prevx;
+  private String prevy;
+  private List<List<TileDTO>> tiles;
 
-
-	public MapDTO(List<List<Sector>> sectors, int x, int y, int id) {
+  public MapDTO(List<List<Sector>> sectors, int x, int y, int id) {
     this.id = id;
-		this.x = x;
-		this.y = y;
-		this.nextx = Validator.format(x+1, y);
-		this.nexty = Validator.format(x, y+1);
-		this.prevx = Validator.format(x-1, y);
-		this.prevy = Validator.format(x, y-1);
-		this.tiles = new ArrayList<>();
-		for(List<Sector> row: sectors) {
-			List<TileDTO> tileRow = new ArrayList<>();
-			tiles.add(tileRow);
-			for(Sector s: row) {
-				tileRow.add(new TileDTO(s));
-			}
-		}
-	}
+    this.x = x;
+    this.y = y;
+    this.nextx = Validator.format(x+1, y);
+    this.nexty = Validator.format(x, y+1);
+    this.prevx = Validator.format(x-1, y);
+    this.prevy = Validator.format(x, y-1);
+    this.tiles = new ArrayList<>();
+    for(List<Sector> row: sectors) {
+      List<TileDTO> tileRow = new ArrayList<>();
+      tiles.add(tileRow);
+      for(Sector s: row) {
+        tileRow.add(new TileDTO(s));
+      }
+    }
+  }
 
-	public int getX() {
-		return x;
-	}
+  public int getX() {
+    return x;
+  }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+  public void setX(int x) {
+    this.x = x;
+  }
 
-	public int getY() {
-		return y;
-	}
+  public int getY() {
+    return y;
+  }
 
-	public void setY(int y) {
-		this.y = y;
-	}
+  public void setY(int y) {
+    this.y = y;
+  }
 
-	public String getNextx() {
-		return nextx;
-	}
+  public String getNextx() {
+    return nextx;
+  }
 
-	public void setNextx(String nextx) {
-		this.nextx = nextx;
-	}
+  public void setNextx(String nextx) {
+    this.nextx = nextx;
+  }
 
-	public String getNexty() {
-		return nexty;
-	}
+  public String getNexty() {
+    return nexty;
+  }
 
-	public void setNexty(String nexty) {
-		this.nexty = nexty;
-	}
+  public void setNexty(String nexty) {
+    this.nexty = nexty;
+  }
 
-	public String getPrevx() {
-		return prevx;
-	}
+  public String getPrevx() {
+    return prevx;
+  }
 
-	public void setPrevx(String prevx) {
-		this.prevx = prevx;
-	}
+  public void setPrevx(String prevx) {
+    this.prevx = prevx;
+  }
 
-	public String getPrevy() {
-		return prevy;
-	}
+  public String getPrevy() {
+    return prevy;
+  }
 
-	public void setPrevy(String prevy) {
-		this.prevy = prevy;
-	}
+  public void setPrevy(String prevy) {
+    this.prevy = prevy;
+  }
 
-	public List<List<TileDTO>> getTiles() {
-		return tiles;
-	}
+  public List<List<TileDTO>> getTiles() {
+    return tiles;
+  }
 
-	public void setTiles(List<List<TileDTO>> tiles) {
-		this.tiles = tiles;
-	}
+  public void setTiles(List<List<TileDTO>> tiles) {
+    this.tiles = tiles;
+  }
 
   public int getId() {
     return id;
--- webapp/src/main/java/ar/edu/itba/paw/webapp/DTOs/TileDTO.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/DTOs/TileDTO.java
@@ -6,57 +6,57 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TileDTO {
 
-	private int x;
-	private int y;
-	private int type;
+  private int x;
+  private int y;
+  private int type;
   private int level;
-	private int owner;
+  private UserDTO owner;
 
-	public TileDTO(Sector sector) {
-		this.x = sector.getPosition().getX();
-		this.y = sector.getPosition().getY();
-		this.type = sector.getType();
-		this.level = sector.getLevel();
-		this.owner = sector.getUser() == null ? -1 : sector.getUser().getId();
-	}
+  public TileDTO(Sector sector) {
+    this.x = sector.getPosition().getX();
+    this.y = sector.getPosition().getY();
+    this.type = sector.getType();
+    this.level = sector.getLevel();
+    this.owner = sector.getUser() == null ? null : new UserDTO(sector.getUser());
+  }
 
-	public int getX() {
-		return x;
-	}
+  public int getX() {
+    return x;
+  }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+  public void setX(int x) {
+    this.x = x;
+  }
 
-	public int getY() {
-		return y;
-	}
+  public int getY() {
+    return y;
+  }
 
-	public void setY(int y) {
-		this.y = y;
-	}
+  public void setY(int y) {
+    this.y = y;
+  }
 
-	public int getType() {
-		return type;
-	}
+  public int getType() {
+    return type;
+  }
 
-	public void setType(int type) {
-		this.type = type;
-	}
+  public void setType(int type) {
+    this.type = type;
+  }
 
   public int getLevel() {
-	  return this.level;
+    return this.level;
   }
 
   public void setLevel(int level) {
-	  this.level = level;
+    this.level = level;
   }
 
-	public int getOwner() {
-		return owner;
-	}
+  public UserDTO getOwner() {
+    return owner;
+  }
 
-	public void setOwner(int owner) {
-		this.owner = owner;
-	}
+  public void setOwner(UserDTO owner) {
+    this.owner = owner;
+  }
 }
